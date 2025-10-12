7132c1d423d577f6484ea6c78db029d2b08ff259
Renata <renata@example.com>
2025-10-12 11:45:48 +0100

    Update

--- tester/test_strlcpy.c
+++ tester/test_strlcpy.c
@@ -4,18 +4,47 @@
 
 void	test_strlcpy(void)
 {
-	char src[10] = "42Porto";
-	char dest1[30];
-	char dest2[30];
+	int	passed = 0;
+	int	failed = 0;
 
-	strlcpy(dest1, src, 7 * sizeof(char));
-	ft_strlcpy(dest2, src, 7 * sizeof(char));
+	typedef struct s_strlcpy_case
+	{
+		char src[30];
+		char dest[30];
+		size_t n;
+	} t_strlcpy_case;
+	
+	t_strlcpy_case tests[] = {
+		{"42 Porto", "Copy here", 20},
+		{"world", "", 3},
+		{"", "Hi", 3},
+		{"Hello", "*****", 2},
+		{"42 Porto", "Copy", 0},
+	};
+
+	int NUM_TESTS = sizeof(tests) / sizeof(tests[0]);
 
 	printf("Testing ft_strlcpy:\n");
 
-	if (strcmp(dest1, dest2) == 0)
-		printf("✅ ft_strlcpy passed\n");
-	else
-		printf("❌ ft_strlcpy failed\n");
-	printf("\n\n");
+	for (int i = 0; i < NUM_TESTS; i++)
+	{
+		char dest1[20], dest2[20], src[20];
+		strcpy(dest1, tests[i].dest);
+		strcpy(dest2, tests[i].dest);
+		strcpy(src, tests[i].src);
+		strlcpy(src, dest1, tests[i].n);
+		ft_strlcpy(src, dest2, tests[i].n);
+
+		if (strcmp(dest1, dest2) == 0)
+		{
+			printf("✅ ft_strlcpy passed\n");
+			passed++;
+		}
+		else
+		{	
+			printf("❌ ft_strlcpy failed\n");
+			failed++;
+		}
+	}
+	printf("\nSummary: %d passed, %d failed\n\n\n", passed, failed);
 }
--- tester/test_strncmp.c
+++ tester/test_strncmp.c
@@ -4,17 +4,52 @@
 
 void	test_strncmp(void)
 {
-	char s1[10] = "42 Porto";
-	char s2[10] = "42 Porta";
-	int res1;
-	int res2;
+	int passed = 0;
+	int failed = 0;
 
-	res1 = strncmp(s1, s2, 5);
-	res2 = ft_strncmp(s1, s2, 5);
+	typedef struct s_strncmp_case
+	{
+		char s1[10];
+		char s2[10];
+		size_t n;
+	} t_strncmp_case;
+	
+	t_strncmp_case tests[] = {
+		{"abcdef", "abcdef", 6},
+		{"abcdef", "axcdef", 2},
+		{"abcdef", "abxyz", 2},
+		{"abcdef", "abcxyz", 2},
+		{"abc", "abcdef", 5},
+		{"abc", "abc", 0},
+		{"", "", 2},
+		{"", "abc", 3},
+		{"abc", "", 3}
+	};
 
-	if (res1 == res2)
-		printf("✅ ft_strncmp passed\n");
-	else
-		printf("❌ ft_strncmp failed\n");
-	printf("\n\n");
+	int NUM_TESTS = sizeof(tests) / sizeof(tests[0]);
+
+	printf("Testing ft_strncmp:\n");
+
+	for (int i = 0; i < NUM_TESTS; i++)
+	{
+		int res1;
+		int res2;
+		char s1[20], s2[20];
+		strcpy(s1, tests[i].s1);
+		strcpy(s2, tests[i].s2);
+		res1 = strncmp(s1, s2, tests[i].n);
+		res2 = ft_strncmp(s1, s2, tests[i].n);
+
+		if (res1 == res2)
+		{
+			printf("✅ ft_strncmp passed\n");
+			passed++;
+		}
+		else
+		{
+			printf("❌ ft_strncmp failed\n");
+			failed++;
+		}
+	}
+	printf("\nSummary: %d passed, %d failed\n\n\n", passed, failed);
 }
